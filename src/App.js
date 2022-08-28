9ccbf340a71df418786076410eb24798f87a38a6
Cordero Dixon <lab@Corderos-MacBook-Pro.local>
2022-08-28 18:48:03 -0400

    style some of the about me section

--- src/App.js
+++ src/App.js
@@ -10,24 +10,25 @@ class App extends Component {
     return (
       <div>
         <nav>
+          <a className="w3-xlarge fa fa-home" href="/"></a>
           <a className="active" href="#about">About</a>
           <a href="#experience">Experience</a>
           <a href="#work">Work</a>
           <a href="#contact">Contact</a>
           <a className="resume-button" href="/Resume_2022.pdf">Resume</a>
         </nav>
-        <div>
-          <section className="header-section">
-            <h1>Hello, my name is</h1>
-            <h2 className="heading"> Cordero Dixon.</h2>
-            <h3 className="heading">I assemble web applications.</h3>
-            <p className="heading-paragraph">I'm a software engineer working on both the front and back-end. Currently, I'm doing some freelance work.</p>
-          </section>
-          <section id="about" className="about-section"><About /></section>
+      <div>
+        <section className="header-section">
+          <h1>Hello, my name is</h1>
+          <h2 className="big-heading"> Cordero Dixon.</h2>
+          <h3 className="big-heading">I assemble web apps.</h3>
+          <p className="heading-paragraph">I'm a software engineer working on both the front and back-end. Currently, I'm doing some freelance work.</p>
+        </section>
+          <section id="about" className="about-section"><About /></section>      
           <section id="experience"><Experience /></section>
           <section id="work"><Work /></section>
           <section id="contact" className="contact-section"><Contact /></section>
-            <h1>Created by Cordero Dixon</h1>
+          <h1>Created by Cordero Dixon</h1>
         </div>
       </div>
 
--- src/components/About.js
+++ src/components/About.js
@@ -3,24 +3,28 @@ import React from 'react';
 class About extends React.Component {
   render(){
     return (
-      <div>
-        <h1>About Me</h1>
-        <p>Yo! My name is Cordero and I love creating things. My interest in web development started back in summer of 2018, so I took a course at General Assembly that taught me my skills.</p>
-        <p>Technologies I work with:</p>
-        <ul className="skills-list">
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Node.js</li>
-          <li>Express.js</li>
-          <li>Ruby on Rails</li>
-          <li>jQuery</li>
-          <li>MongoDB</li>
-          <li>Angular.js</li>
-          <li>Postgres</li>
-          <li>SQL</li>
-        </ul>
+      <div className="inner">
+        <div>
+          <h2 className="heading">About Me</h2>
+          <div>
+            <p>Yo! My name is Cordero and I love creating things. My interest in web development started back in summer of 2018, so I took a course at General Assembly that taught me my skills.</p>
+            <p>Technologies I work with:</p>
+          </div>
+          <ul className="skills-list">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>Ruby on Rails</li>
+            <li>jQuery</li>
+            <li>MongoDB</li>
+            <li>Angular.js</li>
+            <li>Postgres</li>
+            <li>SQL</li>
+          </ul>
+        </div>
       </div>
     )
   }
