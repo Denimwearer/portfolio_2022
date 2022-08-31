083039706d042215ff675c5da85d3d330b986e95
Cordero Dixon <lab@Corderos-MacBook-Pro.local>
2022-08-31 06:53:03 -0400

    added some more styles to work section

--- src/components/Work.js
+++ src/components/Work.js
@@ -10,13 +10,13 @@ class Work extends React.Component {
             <div className="project-content">
               <div>
                 <p className="project-overline">Featured Project</p>
-                <h3>
+                <h3 className="project-title">
                   <a href="https://quitter-app.herokuapp.com/">Quitter</a>
                 </h3>
-                <div>
-                  <p>Quitter is a social media app for the non-committal type. Feel free to communicate with everyone on Quitter about the things that you've failed to get done. We would love to chat... wait, I'm just going to stop there. I've told you enough.</p>
+                <div className="project-description">
+                  <p>Social media app for the non-committal type. Feel free to communicate with everyone on Quitter about the things that you've failed to get done. We would love to chat... wait, I'm just going to stop there. I've told you enough.</p>
                 </div>
-                <ul>
+                <ul className="technologies-used-list">
                   <li>React</li>
                   <li>Ruby on Rails</li>
                   <li>Postgres</li>
@@ -24,14 +24,20 @@ class Work extends React.Component {
                   <li>HTML</li>
                   <li>CSS</li>
                 </ul>
-                <div>
-                  <a href="https://github.com/Denimwearer/Quitter"> </a>
-                  <a href="https://quitter-app.herokuapp.com/"> </a>
+                <div className="project-links">
+                  <a href="https://github.com/Denimwearer/Quitter"><i className="fa-brands fa-github"></i></a>
+                  <a href="https://quitter-app.herokuapp.com/"><i className="fa-solid fa-link"></i></a>
                 </div>
               </div>
             </div>
-            <div>
-              <a href="https://quitter-app.herokuapp.com/"> </a>
+            <div className="project-image">
+              <a href="https://quitter-app.herokuapp.com/">
+                <div className="image-wrapper image-wrapper-constrained image">
+                  <div className="work-image-div">
+                    <img src="Quitter.png"></img>
+                  </div>
+                </div>
+              </a>  
             </div>
           </li>
         </ul>
