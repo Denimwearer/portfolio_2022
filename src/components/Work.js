78c20f3d8c16397241042e56186274df86d6773a
Cordero Dixon <lab@Corderos-MacBook-Pro.local>
2022-11-17 08:32:00 -0500

    fixed typo

--- src/components/Work.js
+++ src/components/Work.js
@@ -16,7 +16,7 @@ class Work extends React.Component {
 
     { id:2,
       name:"Playlistr",
-      description: "App for creating customizable and social playlist. It uses the Last.fm API to search from a catalog and music and features a filter to allow you to serach for specific keywords in titles or artists.",
+      description: "App for creating customizable and social playlist. It uses the Last.fm API to search from a catalog and music and features a filter to allow you to search for specific keywords in titles or artists.",
       technologies: ["Angular.js", "Express.js", "MongoDB", "JavaScript", "HTML", "CSS", "Node.js"],
       links:["https://github.com/Denimwearer/Playlistr", "https://csa-music-app.herokuapp.com/"],
       picture: "Playlistr.png"
